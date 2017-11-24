241f2afda63e0d875ad44940822b7f291382dff0
Paco Chacon <pacochacon94@gmail.com>
2017-11-24 11:38:21 -0600

    Added login view controller

---
+++ Rebuc/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Rebuc
+//
+//  Created by Paco Chacon de Dios on 24/11/17.
+//  Copyright © 2017 Paco Chacon de Dios. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
