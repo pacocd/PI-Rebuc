e1fbf1fa89eaa5c189bbe321835696622c2a3e6f
Paco Chacon <pacochacon94@gmail.com>
2017-11-24 13:13:22 -0600

    Set login as initial view

--- Rebuc/AppDelegate.swift
+++ Rebuc/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setInitialFlow()
         return true
     }
 
@@ -41,6 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
 
+extension AppDelegate {
+
+    func setInitialFlow() {
+        window?.rootViewController = UIViewController().instantiate(viewController: "LoginViewController", storyboard: "Authentication")
+    }
+
+}
