1cac36fcf8be66b38a1ec01cd6fe62bb1b951f9d
Paco Chacon <pacochacon94@gmail.com>
2017-11-27 14:07:05 -0600

    Added ticketsviewcontroller

---
+++ Rebuc/TicketsListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TicketsListViewController.swift
+//  Rebuc
+//
+//  Created by Paco Chacon de Dios on 27/11/17.
+//  Copyright © 2017 Paco Chacon de Dios. All rights reserved.
+//
+
+import UIKit
+
+class TicketsListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
