2af25cd8a36db12d913c13bbefcd5211ea210922
Paco Chacon <pacochacon94@gmail.com>
2017-11-27 18:05:59 -0600

    Changed user role default for sign up and added gray table view separator color

--- Rebuc/Extensions.swift
+++ Rebuc/Extensions.swift
@@ -33,6 +33,10 @@ extension UIColor {
         return UIColor(red: 57 / 255, green: 165 / 255, blue: 83 / 255, alpha: 1)
     }
 
+    class var grayTableViewSeparator: UIColor {
+        return UIColor(red: 217 / 255, green: 217 / 255, blue: 217 / 255, alpha: 1)
+    }
+
 }
 
 extension Notification.Name {
