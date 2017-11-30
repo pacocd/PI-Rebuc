cbe2108b8687135e69b5488db58ae29104e3a53b
Paco Chacon <pacochacon94@gmail.com>
2017-11-30 01:25:34 -0600

    Redefine protocols

--- Rebuc/Protocols.swift
+++ Rebuc/Protocols.swift
@@ -8,10 +8,24 @@
 
 import Foundation
 
-@objc protocol Model {
+
+/// Model protocol using ModelProperty, ModelURL and ModelNode
+@objc protocol Model: ModelProperty, ModelURL, ModelNode {
+}
+
+/// ModelProperty protocol defines basic properties for Models
+@objc protocol ModelProperty {
     var id: Int { get set }
     @objc optional var name: String { get set }
+}
+
+/// ModelURL protocol defines to get URL for current Model
+@objc protocol ModelURL {
     static func getUrl() -> String
+}
+
+/// ModelNode protocol defines to get node name if is singular or plural
+@objc protocol ModelNode {
     static func singularNodeName() -> String
     static func pluralNodeName() -> String
 }
